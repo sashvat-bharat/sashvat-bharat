6742825d365f4f4612d8b7ac7a28545b08034d1e
Akshat Diwedi <nuclear.games111@gmail.com>
2026-03-03 16:36:06 +0530

    Minor Changes and Fixes.

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -11,19 +11,22 @@ const HeroSection = () => {
         <div className='label'>
           <div className='dot'></div>
           <p>Introducing: JIT Tool Spawning Protocol</p>
-          {/* <p>Introducing Genesis</p> */}
-          {/* <p>Introducing Genesis, try now</p> */}
           <MoveRight strokeWidth={1} />
         </div>
 
 
         <div className='intro-text'>
-          {/* <h1>Legacy Tech is a Liability. Build Something Sashvat.</h1> */}
-          <h1>Rule the Horizon. Beyond Limits.</h1>
-          <p>We’re building a new generation of technology that’s sustainable, ethical, and designed for the long haul. Because the best ideas deserve to last.</p>
+          <h1>Rule the Horizon. Beyond Limits!</h1>
+          <p>Built to last. Designed to lead. Engineered for the future.</p>
         </div>
 
-        <button className="button_w_icon">Experience Now</button>
+
+        <center>
+          <button className="event-horizon-btn">
+            <span className="btn-text">Enter the Horizon</span>
+            <div className="btn-underline"></div>
+          </button>
+        </center>
 
         <div className="hero-image">
           <Image src="/ascii.svg" alt="hero-image" width={100} height={100} />
