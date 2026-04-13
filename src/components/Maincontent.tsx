8df6017b45be6231a0c4765ccc67982ad3b53553
Akshat-Dwivedi-52 <nuclear.games111@gmail.com>
2026-04-13 23:13:56 +0530

    fix: adjust hero section heading formatting and chevron animation offset

--- src/components/Maincontent.tsx
+++ src/components/Maincontent.tsx
@@ -9,9 +9,7 @@ export default function Maincontent() {
         <Image className="background_image" src="/ascii.svg" alt="Logo" width={100} height={100} quality={100} />
 
         <div className="landing_components">
-          <h1 className="landing_heading">
-            Legacy Tech is a Liability <br /> so we're Building Something Sashvat.
-          </h1>
+          <h1 className="landing_heading">Legacy Tech is a Liability <br /> so we&apos;re Building Something Sashvat.</h1>
         </div>
       </div>
 
