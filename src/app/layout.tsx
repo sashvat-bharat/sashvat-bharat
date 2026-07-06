cd5340a50d5b23a9d3672fbfe3c1c8ff252f629e
Akshat-Dwivedi-52 <nuclear.games111@gmail.com>
2026-07-06 21:18:15 +0530

    style: update libreBaskerville font import to include style options for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "katex/dist/katex.min.css";
 
 // Fonts Imports -------------------------
 
-const libreBaskerville = Libre_Baskerville({ variable: "--font-libre-baskerville", subsets: ["latin"], weight: "400" });
+const libreBaskerville = Libre_Baskerville({ variable: "--font-libre-baskerville", subsets: ["latin"], weight: ["400"], style: ["normal", "italic"] });
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"], });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"], });
 const sourceSerif4 = Source_Serif_4({ variable: "--font-source-serif-4", subsets: ["latin"], });
